601a4cf7afa16fbe3d4f61a785f308a11d85c65b
Sathyapriya52 <157713145+Sathyapriya52@users.noreply.github.com>
2024-02-13 19:56:45 +0530

    Add files via upload

---
+++ week3/day2feb06/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+package myAssignments.week3.day2feb06;
+
+public class RemoveDuplicates {
+	public static void main(String[] args) {
+		
+	
+	//Declare a string
+	String text = "We learn java basics as part of java sessions in java week1";
+	//Initialize an integer variable
+	int count;
+	//Split the string into array
+	String[] characters =text.split(" ");
+	//Create two nested for loops to compare each word with every other word in the String array. 
+	for(int i=0;i<length.text;i++)
+		
+	
+	}
+}
